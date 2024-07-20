8565c82a006409458290d14aa45c1f5644fe36bd
angel <angel.saez.2002@gmail.com>
2024-07-20 16:16:54 +0200

    Modificado index, style y script

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-// script.js
 document.addEventListener('DOMContentLoaded', (event) => {
     const toggleButton = document.getElementById('toggleButton');
+    const toggleIcon = document.getElementById('toggleIcon');
+    const header = document.querySelector('header');
+    const main = document.querySelector('main');
+    const body = document.querySelector('body');
+
+
     toggleButton.addEventListener('click', () => {
-        document.body.classList.toggle('dark-mode');
-        if (document.body.classList.contains('dark-mode')) {
-            toggleButton.textContent = 'Modo Claro';
+        header.classList.toggle('dark-mode-header');
+        main.classList.toggle('dark-mode-main');
+        body.classList.toggle('dark-mode-body');
+        
+        if (header.classList.contains('dark-mode-header')) {
+            toggleIcon.src = 'images/dark-mode.ico';
+            toggleIcon.alt = 'Modo Claro';
         } else {
-            toggleButton.textContent = 'Modo Oscuro';
+            toggleIcon.src = 'images/light-mode.ico';
+            toggleIcon.alt = 'Modo Oscuro';
         }
     });
 });
